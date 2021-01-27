be0f857284cc2be54b85b12b8e6d235bbb6afc6f
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-01-27 22:22:48 +0200

    Tehtävä 1.7 unicafe step2

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -12,6 +12,16 @@ const App = () => {
   const neutralClick = () => setNeutral(neutral + 1)
   const badClick = () => setBad(bad + 1)
 
+  const feedbackAll = () => (good + neutral + bad)
+  const feedbackAverage = () => {
+    if ((good + neutral + bad) === 0) return 0
+    return (good - bad)/(good + neutral + bad)
+  }
+  const feedbackPositivePercent = () => {
+    if ((good + neutral + bad) === 0) return 0
+    return ((good)/(good + neutral + bad))*100
+  }
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -22,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {feedbackAll()}</p>
+      <p>average {feedbackAverage()}</p>
+      <p>positive {feedbackPositivePercent()} %</p>
     </div>
   )
 }
