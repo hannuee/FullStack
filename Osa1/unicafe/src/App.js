300550da9aa78343faa5bda10bdd7d8e7ae2cf5d
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-01-27 22:37:42 +0200

    Tehtävä 1.8 unicafe step3

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -2,16 +2,7 @@ import React, { useState } from 'react'
 
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const goodClick = () => setGood(good + 1)
-  const neutralClick = () => setNeutral(neutral + 1)
-  const badClick = () => setBad(bad + 1)
-
+const Statistics = ({good, neutral, bad}) => {
   const feedbackAll = () => (good + neutral + bad)
   const feedbackAverage = () => {
     if ((good + neutral + bad) === 0) return 0
@@ -23,11 +14,7 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Give feedback</h1>
-      <Button text="good" handleClick={goodClick} />
-      <Button text="neutral" handleClick={neutralClick} />
-      <Button text="bad" handleClick={badClick} />
+    <>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -35,6 +22,27 @@ const App = () => {
       <p>all {feedbackAll()}</p>
       <p>average {feedbackAverage()}</p>
       <p>positive {feedbackPositivePercent()} %</p>
+    </>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => setGood(good + 1)
+  const neutralClick = () => setNeutral(neutral + 1)
+  const badClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="good" handleClick={goodClick} />
+      <Button text="neutral" handleClick={neutralClick} />
+      <Button text="bad" handleClick={badClick} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
