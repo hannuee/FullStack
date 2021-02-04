933d9cd6844696a1a5e84699b344da8a3c342bb5
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-02-04 19:04:47 +0200

    Tehtävä 2.5 erillinen moduuli

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -1,38 +1,5 @@
 import React from 'react'
-
-const Header = (props) => (
-  <>
-    <h2>{props.courseName}</h2>
-  </>
-)
-
-const Part = (props) => (
-  <>
-    <p>
-       {props.part} {props.exercises}
-    </p>
-  </>
-)
-
-const Content = (props) => (
-  <>
-    {props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-  </>
-)
-
-const Total = (props) => (
-  <>
-    <p><b>total of {props.parts.reduce((acc, cur) => acc + cur.exercises, 0)} exercises</b></p>
-  </>
-)
-
-const Course = ({course}) => (
-  <>
-    <Header courseName={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
