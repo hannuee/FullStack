f8bbd9838b7488f300a1f336273d2a711ed9a605
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-02-08 17:08:17 +0200

    Tehtävä 2.11 puhelinluettelo step6

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterForm = ({search, handleSearchChange}) => (
   <div> filter shown with: <input value={search} onChange={handleSearchChange} /></div>
@@ -19,15 +20,16 @@ const PersonsList = ({personsToShow}) => (
 )
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
+
+  useEffect(() => { 
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+    }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
